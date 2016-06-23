0454b31b7e9d8563e729c7d2daa841ad99ae7571
UnoiaTech <unoiaofficial@gmail.com>
2016-06-23 17:43:27 +0530

    New commit

---
+++ LoginButtonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginButtonDemo
+//
+//  Created by Unoiatech on 20/06/16.
+//  Copyright © 2016 Unoiatech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginButtonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginButtonDemo
+//
+//  Created by Unoiatech on 20/06/16.
+//  Copyright © 2016 Unoiatech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
